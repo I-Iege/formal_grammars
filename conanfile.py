4dfd28f9ec752bbeb5949c0f0eed113224e424a4
Attila <hegedus.attila2@gmail.com>
2024-12-28 12:18:24 +0100

    update generators

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,10 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
 
-#from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
-
-
 class Package(ConanFile):
 
     settings = 'os', 'build_type', 'compiler'
-    generators = "cmake", "cmake_paths", "cmake_find_package"
+    generators = "CMakeDeps"
 
     def requirements(self):
         self.tool_requires("qt/6.7.0")
