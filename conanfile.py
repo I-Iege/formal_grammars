2e36c674fe28da4f21a6b3eec1a7f6a8f1ba62b0
Attila <hegedus.attila2@gmail.com>
2024-12-28 11:45:33 +0100

    change to conan 2

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,17 @@
-from conans import ConanFile, CMake
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+
 #from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
 
 
 class Package(ConanFile):
 
-    settings = 'os', 'build_type'
-
+    settings = 'os', 'build_type', 'compiler'
     generators = "cmake", "cmake_paths", "cmake_find_package"
     
     def requirements(self):
-        self.requires("qt/6.4.2")
-        self.requires("gtest/1.13.0")
+        self.tool_requires("qt/6.7.0")
+        self.test_requires("gtest/1.13.0")
     
     def build(self):
         cmake = CMake(self)
